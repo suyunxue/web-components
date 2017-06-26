2255c5fa43a485813080249381a36f23bc98ba20
sufubo <731644018@qq.com>
2017-06-26 17:27:27 +0800

    hsl转化rgb出现问题

--- colorSelect/js/lib.js
+++ colorSelect/js/lib.js
@@ -20,6 +20,34 @@ function rgbToHsl(r, g, b){
 }
 
 
+function  hslToRgb(h, s, l) {
+  h = h / 360;
+  var r, g, b;
+  if (s == 0) {
+      r = g = b = l;
+  } else {
+      function hue2rgb(p, q, t) {
+          if (t < 0) t += 1;
+          if (t > 1) t -= 1;
+          if (t < 1 / 6) return p + (q - p) * 6 * t;
+          if (t < 1 / 2) return q;
+          if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+          return p;
+      }
+      if(l<0.5){
+        var q=l * (1 + s);
+      } else{
+        var q=l + s - l * s;
+      }
+
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+  }
+  return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+}
+
 function hslToRgb(h, s, l){
     var r, g, b;
 
